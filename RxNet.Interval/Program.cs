564ee37375ee65527512e105ea84cd91fe5f1164
Peter <sheraium@gmail.com>
2021-01-29 08:23:11 +0800

    Add project files.

---
+++ RxNet.Interval/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RxNet.Interval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
