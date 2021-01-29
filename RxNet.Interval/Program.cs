ac987f479ed9ced635b96834e18fbd64153ebf54
Peter <sheraium@gmail.com>
2021-01-29 09:47:37 +0800

    測試 Subscribe Disposable

--- RxNet.Interval/Program.cs
+++ RxNet.Interval/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 
 namespace RxNet.Interval
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var disposables = new List<IDisposable>();
+            foreach (var i in Enumerable.Range(1, 10))
+            {
+                var target = new Target();
+                disposables.Add(target);
+            }
+
+            Console.WriteLine("Create OK");
+            Console.ReadLine();
+            disposables.ForEach(disposable => disposable.Dispose());
+
+            Console.WriteLine("Wait Exit");
+            Console.ReadLine();
+        }
+    }
+
+    public class Target : IDisposable
+    {
+        private IDisposable _disposable;
+
+        public Target()
+        {
+            _disposable = Observable
+                .Interval(TimeSpan.FromMilliseconds(1_000))
+                .ObserveOn(TaskPoolScheduler.Default.DisableOptimizations(typeof(ISchedulerLongRunning)))
+                .Subscribe(l =>
+                {
+                    Console.WriteLine(l);
+                });
+        }
+
+        public void Dispose()
+        {
+            _disposable?.Dispose();
         }
     }
 }
