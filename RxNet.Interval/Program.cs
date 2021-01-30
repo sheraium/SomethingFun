9080a976b2f1a24d077c2ecae3b736b235bd4cbc
Peter <sheraium@gmail.com>
2021-01-31 00:58:14 +0800

    test Observable Async/Await

--- RxNet.Interval/Program.cs
+++ RxNet.Interval/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace RxNet.Interval
 {
@@ -10,6 +13,13 @@ namespace RxNet.Interval
     {
         private static void Main(string[] args)
         {
+            Observable
+                .Interval(TimeSpan.FromMilliseconds(1))
+                .Select(l => Observable.FromAsync(() => GetInt(l)))
+                .Concat()
+                .Subscribe();
+            Console.ReadLine();
+
             var disposables = new List<IDisposable>();
             foreach (var i in Enumerable.Range(1, 10))
             {
@@ -24,6 +34,15 @@ namespace RxNet.Interval
             Console.WriteLine("Wait Exit");
             Console.ReadLine();
         }
+
+        private static async Task GetInt(long value)
+        {
+            Console.WriteLine(value);
+            Console.WriteLine(DateTime.Now.ToString("hh:mm:ss.fff"));
+            await Task.Delay(2000);
+            //return default;
+            //SpinWait.SpinUntil(() => false, 2000);
+        }
     }
 
     public class Target : IDisposable
@@ -35,10 +54,7 @@ namespace RxNet.Interval
             _disposable = Observable
                 .Interval(TimeSpan.FromMilliseconds(1_000))
                 .ObserveOn(TaskPoolScheduler.Default.DisableOptimizations(typeof(ISchedulerLongRunning)))
-                .Subscribe(l =>
-                {
-                    Console.WriteLine(l);
-                });
+                .Subscribe(l => { Console.WriteLine(l); });
         }
 
         public void Dispose()
