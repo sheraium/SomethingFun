652e2447c34b303a32d1e7466afbdb88d364fe16
Chen YongXiu <sheraium@gmail.com>
2021-08-03 08:40:54 +0800

    修改 Timer 改使用 Observable Interval

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentNullException("Test");
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            var subject = new Subject<string>();
+            subject
+                .Select(s => Observable.FromAsync(async () =>
+                {
+                    await Task.Delay(2000);
+                    Console.WriteLine($"Subscribe: {s}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+                }))
+                .Concat()
+                .Subscribe();
+
+            subject.OnNext("1");
+            Console.WriteLine("Main: 1");
+            subject.OnNext("2");
+            Console.WriteLine("Main: 2");
+            subject.OnNext("3");
+            Console.WriteLine("Main: 3");
+
+            Console.ReadKey();
+        }
+    }
+}
