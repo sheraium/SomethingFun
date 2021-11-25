266a6f2f15d4caa49ec03f4937a812855519e77a
Chen YongXiu <sheraium@gmail.com>
2021-11-25 14:17:33 +0800

    新增 ZipFileTester

---
+++ ZipFileTester/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace ZipFileTester
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var fileName = "HelloWorld.txt";
+            var bytes = Encoding.UTF8.GetBytes("HelloWorld");
+
+            using (var zipStream = new MemoryStream())
+            {
+                using (var zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Update))
+                {
+                    var entry = zipArchive.CreateEntry(fileName);
+                    using (var entryStream = entry.Open())
+                    {
+                        var buff = bytes;
+                        entryStream.Write(buff, 0, buff.Length);
+                    }
+                }
+
+                Console.WriteLine(BitConverter.ToString(zipStream.ToArray()));
+
+                File.WriteAllBytes("HelloWorld.zip", zipStream.ToArray());
+            }
+        }
+    }
+}
